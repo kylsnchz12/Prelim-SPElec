cd594aa11b69278274e07f29a9e9bc52a5018512
Kyle <kyle_sanchez99@yahoo.com>
2023-02-21 23:37:56 +0800

    added an image with gradient border

--- flutter_prelim/lib/screens/onboading_screen.dart
+++ flutter_prelim/lib/screens/onboading_screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+import 'package:gradient_borders/gradient_borders.dart';
+
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
@@ -52,7 +54,54 @@ class OnboardingScreen extends StatelessWidget {
                       color: Colors.transparent,
                     ),
                   ),
-                ))
+                )),
+            SafeArea(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: screenHeight * 0.07,
+                ),
+                Container(
+                  width: 328,
+                  height: 328,
+                  decoration: BoxDecoration(
+                      image: const DecorationImage(
+                          image: AssetImage("images/image81.png"),
+                          fit: BoxFit.cover),
+                      border: GradientBoxBorder(
+                        gradient: LinearGradient(
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                            colors: [
+                              const Color(0xFFFE53BB).withOpacity(0.4),
+                              const Color(0xFFFE53BB).withOpacity(0),
+                              const Color(0xFF09FBD3).withOpacity(0.1),
+                              const Color(0xFF09FBD3).withOpacity(0.4)
+                            ],
+                            stops: const [
+                              0.2,
+                              0.4,
+                              0.6,
+                              1
+                            ]),
+                        width: 4,
+                      ),
+                      borderRadius: BorderRadius.circular(200)),
+                ),
+                SizedBox(height: screenHeight * 0.09),
+                Text(
+                  'Watch movies in\nvirtual reality',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 34,
+                      color: Colors.white.withOpacity(0.85),
+                      fontWeight: FontWeight.w700,
+                      fontFamily: 'SFPro'),
+                )
+              ],
+            ))
           ],
         ),
       ),
