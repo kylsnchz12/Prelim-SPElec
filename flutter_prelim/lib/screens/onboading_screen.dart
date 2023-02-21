4897639d54412905fe141e47e8415fee5d7b13cb
Kyle <kyle_sanchez99@yahoo.com>
2023-02-21 22:28:37 +0800

    creating onboarding screen

---
+++ flutter_prelim/lib/screens/onboading_screen.dart
@@ -0,0 +1,61 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: const Color(0xFF19191B),
+      extendBody: true,
+      body: SizedBox(
+        width: screenWidth,
+        height: screenHeight,
+        child: Stack(
+          children: [
+            Positioned(
+                top: screenHeight * 0.1,
+                left: -88,
+                child: Container(
+                  height: 166,
+                  width: 166,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Color(0xFFFE53BB)),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 200, sigmaY: 200),
+                    child: Container(
+                      height: 166,
+                      width: 166,
+                      color: Colors.transparent,
+                    ),
+                  ),
+                )),
+            Positioned(
+                top: screenHeight * 0.3,
+                right: -110,
+                child: Container(
+                  height: 200,
+                  width: 200,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Color(0xFF09FBD3)),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 200, sigmaY: 200),
+                    child: Container(
+                      height: 200,
+                      width: 200,
+                      color: Colors.transparent,
+                    ),
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
